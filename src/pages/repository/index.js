f805f65b1361f8cab168932808c0a37a5ffe5a70
Arievilo <jadieoliveira00@gmail.com>
2019-09-23 12:47:20 -0300

    Carregando dados da API

--- src/pages/repository/index.js
+++ src/pages/repository/index.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
 import api from '../../sevice/api';
 
 class Repository extends Component {
+  // eslint-disable-next-line
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   state = {
     repository: {},
     issues: [],
@@ -19,12 +29,13 @@ class Repository extends Component {
 
     this.setState({
       repository: repository.data,
-      issues: repository.data,
+      issues: issues.data,
       loading: false,
     });
   }
 
   render() {
+    // eslint-disable-next-line
     const { repository, issues, loading } = this.state;
     return <h1>Repository:</h1>;
   }
